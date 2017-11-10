5dd9d43b3b536a909cbffe8e66d1bfc0a067dfb2
AndroPlus <androplus@andro.plus>
2017-11-10 22:51:40 +0900

    Change build script

--- build.sh
+++ build.sh
@@ -1,148 +1,35 @@
-#!/bin/bash
-rm .version
-# Bash Color
-green='\033[01;32m'
-red='\033[01;31m'
-blink_red='\033[05;31m'
-restore='\033[0m'
+#!/bin/sh
 
-clear
+# This is a temporary, simple build script until twrp is ready for anykernel
 
-# Resources
-THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
-KERNEL="Image"
-DTBIMAGE="dtb"
-export CLANG_PATH=~/android/clang/clang-3859424/bin/
-export PATH=${CLANG_PATH}:${PATH}
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=${HOME}/android/uberbuild/out/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-DEFCONFIG="snoke_defconfig"
-
-# Kernel Details
-VER=".R8"
-
-# Paths
-KERNEL_DIR=`pwd`
-REPACK_DIR="${HOME}/android/AK-OnePone-AnyKernel2"
-PATCH_DIR="${HOME}/android/AK-OnePone-AnyKernel2/patch"
-MODULES_DIR="${HOME}/android/AK-OnePone-AnyKernel2/modules"
-ZIP_MOVE="${HOME}/android/AK-releases"
-ZIMAGE_DIR="${HOME}/android/wahoo/arch/arm64/boot/"
-
-# Functions
-function clean_all {
-		rm -rf $MODULES_DIR/*
-		cd ~/android/wahoo/out/kernel
-		rm -rf $DTBIMAGE
-		git reset --hard > /dev/null 2>&1
-		git clean -f -d > /dev/null 2>&1
-		cd $KERNEL_DIR
-		echo
-		make clean && make mrproper
-}
-
-function make_kernel {
-		echo
-		make CC=clang $DEFCONFIG
-		make CC=clang $THREAD
-
-}
-
-function make_modules {
-		rm `echo $MODULES_DIR"/*"`
-		find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
-}
-
-function make_dtb {
-		$REPACK_DIR/tools/dtbToolCM -2 -o $REPACK_DIR/$DTBIMAGE -s 2048 -p scripts/dtc/ arch/arm64/boot/
-}
-
-function make_boot {
-		cp -vr $ZIMAGE_DIR/Image.lz4-dtb ~/android/wahoo/out/zImage
-}
-
-
-function make_zip {
-		cd ~/android/wahoo/out
-		zip -r9 `echo $AK_VER`.zip *
-		mv  `echo $AK_VER`.zip $ZIP_MOVE
-		cd $KERNEL_DIR
-}
-
-
-DATE_START=$(date +"%s")
-
-
-echo -e "${green}"
-echo "-----------------"
-echo "Making snoke Kernel:"
-echo "-----------------"
-echo -e "${restore}"
-
-
-# Vars
-BASE_AK_VER="snoke"
-AK_VER="$BASE_AK_VER$VER"
-export LOCALVERSION=~`echo $AK_VER`
-export LOCALVERSION=~`echo $AK_VER`
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_BUILD_USER=matthewdalex
-export KBUILD_BUILD_HOST=DarkRoom
+#export CLANG_PATH=~/sdclang/linux-x86/bin/
+export CLANG_PATH=~/Desktop/kernel/wahoo/tools/linux-x86/clang-3859424/bin/
+export PATH=${CLANG_PATH}:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=~/aarch64-linux-android-4.9-aosp/bin/aarch64-linux-android-
+export MKDTBIMG_PATH=~/bin/
+export PATH=${MKDTBIMG_PATH}:${PATH}
+export LD_LIBRARY_PATH=:${LD_LIBRARY_PATH}
 
-echo
+#make mrproper -j8
+make snoke_defconfig
+make CC=clang -j8
+make CC=clang dtbs -j8
 
-while read -p "Do you want to clean stuffs (y/n)? " cchoice
-do
-case "$cchoice" in
-	y|Y )
-		clean_all
-		echo
-		echo "All Cleaned now."
-		break
-		;;
-	n|N )
-		break
-		;;
-	* )
-		echo
-		echo "Invalid try again!"
-		echo
-		;;
-esac
-done
+if [ -f arch/arm64/boot/Image.lz4-dtb ]
+then
 
-echo
-
-while read -p "Do you want to build?" dchoice
-do
-case "$dchoice" in
-	y|Y )
-		make_kernel
-		make_dtb
-		make_modules
-		make_boot
-		break
-		;;
-	n|N )
-		break
-		;;
-	* )
-		echo
-		echo "Invalid try again!"
-		echo
-		;;
-esac
-done
-
-
-echo -e "${green}"
-echo "-------------------"
-echo "Build Completed in:"
-echo "-------------------"
-echo -e "${restore}"
-
-DATE_END=$(date +"%s")
-DIFF=$(($DATE_END - $DATE_START))
-echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
-echo
+cp -a arch/arm64/boot/dtbo.img ../tools/dtbo.img
+../tools/mkbootimg \
+	--kernel arch/arm64/boot/Image.lz4-dtb \
+	--ramdisk ../tools/boot.img-ramdisk.cpio.gz \
+	--cmdline "console=ttyMSM0,115200,n8 earlycon=msm_serial_dm,0xc1b0000 androidboot.hardware=taimen androidboot.console=ttyMSM0 lpm_levels.sleep_disabled=1 user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware loop.max_part=7 buildvariant=user" \
+	--base 0x00000000 \
+        --kernel_offset 0x00008000 \
+        --ramdisk_offset 0x01000000 \
+        --tags_offset 0x00000100 \
+        --pagesize 4096 \
+	--output ../tools/boot.img
+fi
